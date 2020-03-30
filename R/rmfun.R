4f1484b66cac8fc024326f2f4c3991a14373c275
Sean <maden@ohsu.edu>
2020-03-30 12:22:59 -0700

    Update rmfun.R

--- R/rmfun.R
+++ R/rmfun.R
@@ -1,8 +1,12 @@
 #!/usr/bin/env R
 
+# https://stackoverflow.com/questions/16699856/get-website-directory-listing-in-an-r-vector-using-rcurl/17187525
+
 # download files
 url <- "https://recount.bio/data/"
-RCurl::getURL(url, ftp.use)
+rd <- RCurl::getURL(url)
+gsub('.*\"', "", gsub('.*\"', "", rd))
 
-RCurl::getURLContent(url)
+RCurl::getURLContent(url, 
+                     ftp.use.epsv = F)
 RCurl::getURL(url)
