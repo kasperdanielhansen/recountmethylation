fc10f47dbfa234c294eea93d3c5e67e5e6faa6a8
Sean <maden@ohsu.edu>
2020-04-06 13:54:27 -0700

    Update make_h5setest.R

--- scripts/make_h5setest.R
+++ scripts/make_h5setest.R
@@ -14,17 +14,19 @@
 #---------
 
 # filename
+
 # testfn <- "remethdb_h5se-test_gr"
 # lf <- "remethdb_h5se_gr_00-00-01_1583780004"
 # v.ts <- gsub(".*gr\\_", "", lf) # timestamp & version
 
 # subset data
-# testfn <- paste0(c(testfn, v.ts), 
-#                  collapse = "_")
+
+# testfn <- paste0(c(testfn, v.ts), collapse = "_")
 # h5se <- HDF5Array::loadHDF5SummarizedExperiment(lf)
-# h5se.test.gr <- h5se[,c(1:2)]
+# h5se.test.gr <- h5se[c(1:1000),c(1:2)]
 
 # colnames(h5se.test.gr)
+
 # 1] "GSM1038308.1548799666.hlink.GSM1038308_5958154021_R01C01"
 # [2] "GSM1038309.1548799666.hlink.GSM1038309_5958154021_R02C01"
 
@@ -33,6 +35,7 @@
 #------------------
 # exacloud transfer
 #------------------
+# ssh maden@acc.ohsu.edu
 # ssh exacloud.ohsu.edu
 # cd /home/exacloud/lustre1/CompBio/recount_meth
 # scp -r -P 21747 metamaden@96.79.111.125:/home/metamaden/recount-methylation/recountmethylation_manuscript_supplement ./
